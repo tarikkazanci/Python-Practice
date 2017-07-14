44c6313668fb450634e1faae0ac17d663df4956b
Tarik Kazanci <tarikkzc@gmail.com>
2017-07-14 16:46:39 -0400

    updated readme

--- pyhtonapp.py
+++ pyhtonapp.py
@@ -1,10 +1,12 @@
 input('Hello, welcome to my first Awesome Python App')
 
+# Get user input. Whatever user inputs grab that and display to user with some message.
 name = input('What is your name?')
 print('Welcome', name)
 
 python = input("Do you like Python? Y/N")
 
+# Get user input. If user enters "Y" print that else print another one.
 if python == 'Y':
     print("That's cool")
 else:
@@ -18,6 +20,7 @@ else:
     print('Get the hell out of my program!')
     print('Just kidding')
 
+# Get user input. If user enters right answer verify user answer. Otherwise reject user's answer
 
 mathquest = input("What is the result of 5*4*3*2*1?")
 if mathquest == "120":
