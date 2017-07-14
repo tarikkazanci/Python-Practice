800de98d600a589f723849637832159784de5b35
Tarik Kazanci <tarikkzc@gmail.com>
2017-07-14 16:12:45 -0400

    pythonclapp

---
+++ pyhtonapp.py
@@ -0,0 +1,36 @@
+input('Hello, welcome to my first Awesome Python App')
+
+name = input('What is your name?')
+print('Welcome', name)
+
+python = input("Do you like Python? Y/N")
+
+if python == 'Y':
+    print("That's cool")
+else:
+    print("That sucks. You should learn it!")
+
+askgame = input("Let's play a game. Are you ready? Y/N")
+
+if askgame == 'Y':
+    print("Alright")
+else:
+    print('Get the hell out of my program!')
+    print('Just kidding')
+
+
+mathquest = input("What is the result of 5*4*3*2*1?")
+if mathquest == "120":
+    print("Got it!")
+else:
+    print("Are you kidding me?")
+
+input('Here is another question')
+
+lastquest = input("What is the capital city of Turkey?")
+if lastquest == "Ankara":
+    print("You're the best!")
+else:
+    print("Wrong answer")
+
+input('Thank you for joining my app...')
